4ab9f4d52a21cd778bb23976912aeec7e60ee8d1
andrearepellini <repellini.andrea@gmail.com>
2025-12-28 00:35:33 +0100

    Configure CORS on the server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,8 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    proxy: {
-      "/api": `http://${process.env.VITE_API_HOST ?? "localhost"}:8080`,
-    },
   },
 });
