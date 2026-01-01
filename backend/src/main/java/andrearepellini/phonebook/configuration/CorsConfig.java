51ce6fe5a0e06a03d29b80c46f33af3d0cded97b
andrearepellini <repellini.andrea@gmail.com>
2026-01-01 17:44:35 +0100

    Update CORS configuration to specify allowed methods for API endpoints

--- backend/src/main/java/andrearepellini/phonebook/configuration/CorsConfig.java
+++ backend/src/main/java/andrearepellini/phonebook/configuration/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173");
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
             }
         };
     }
